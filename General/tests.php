b2b4d860259297720f4c41c96c07b0ff785921e9
geraldo.tech.dell <geraldo.filho92@hotmail.com>
2024-09-29 10:09:20 -0300

    update

--- General/tests.php
+++ General/tests.php
@@ -42,7 +42,21 @@ $escaped_string = addslashes($string);
     "nome" => 'Geraldo',
     "nome" => 'Felipe',
     "nome" => 'Bella',
-  ]
+  ];
+
+// criar arquivo text
+
+// Obtém o timestamp atual
+$timestamp = date('Ymd_His'); // Formato: '20240927_153000'
+
+// Define o nome do arquivo com o timestamp
+$filename = "teste_$timestamp.txt";
+
+// Escreve no arquivo
+file_put_contents($filename, "Este arquivo foi criado em: $timestamp\n");
+
+// Confirmação de que o arquivo foi criado
+echo "Arquivo '$filename' criado com sucesso!";
 
 ?> 
 <!DOCTYPE html>
