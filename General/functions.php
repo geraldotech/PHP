8b2c419b814a06a6feda55b047b1988ccc6b2c8e
geraldo.tech.dell <geraldo.filho92@hotmail.com>
2024-10-01 20:52:50 -0300

    UPDATE

--- General/functions.php
+++ General/functions.php
@@ -34,10 +34,9 @@ $input = "<h1>meu form</h1>";
 print_r($input);
 print_r("Geraldo");
 echo strip_tags($input, '<br>');
-?>
 
 
- <!-- functions and return by gmapdev -->
+/* functions and return by gmapdev */ 
 
 function checkNum($media){
   if ((float)$media >= 6 ){
@@ -46,8 +45,8 @@ function checkNum($media){
   }
     echo 'Reprovado';
 }
-checkNum(5);
-checkNum(6.4);
+// checkNum(5);
+// checkNum(6.4);
 
 
 function isAppoved($n1, $n2){
@@ -61,4 +60,35 @@ function isAppoved($n1, $n2){
   echo 'Aprovado'. $media;
 }
 
-isAppoved(5.8, 5.7);
+//isAppoved(5.8, 5.7);
+
+
+function consulta($ativo= null){
+
+ // Por padrão, começa ativo
+ $ativoStatus = "AND ativo = 1";
+     
+ if ($ativo === 'todos') {
+     // Se for 'todos', não aplica filtro (busca todos)
+     $ativoStatus = "";
+ }
+  if ($ativo === 'inativo') {
+     // Se for 'inativo', filtra para ativo = 0
+     $ativoStatus = "AND ativo = 0";
+ }
+
+ echo $ativoStatus;
+ return "SELECT users from TEST $ativoStatus";
+}
+
+ echo '<hr>'; 
+
+print_r(consulta('inativo'));
+ echo '<hr>'; 
+ print_r(consulta('todos'));
+ echo '<hr>'; 
+print_r(consulta());
+
+?>
+
+
