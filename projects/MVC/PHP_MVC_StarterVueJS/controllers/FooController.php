e016d59517d2fa2c9bea9f21dc02a94cb38ef0f4
gmapdell <geraldo.filho92@hotmail.com>
2024-10-14 22:19:24 -0300

    UPDATE Vue

--- projects/MVC/PHP_MVC_StarterVueJS/controllers/FooController.php
+++ projects/MVC/PHP_MVC_StarterVueJS/controllers/FooController.php
@@ -5,8 +5,82 @@ class FooController extends Controller {
         parent::__construct();
     }
 
-    public function index() {   
-        $this->loadTemplate('foo');   
+    public function index():void {   
+
+    $myapi['data'] = [
+            [
+                "idLogin" => 2400,
+                "login" => "super",
+                "nomeUsuario" => "super Usuário",
+                "email" => "super@admin.com",
+                "senha" => "1e5213b7cbc4d1780f09770123097879",
+                "validade" => "2025-02-27",
+                "idTotovs" => null,
+                "trocaSenha" => 0,
+                "validadeSenha" => "2025-02-27",
+                "token" => null,
+                "gestorUsuario" => "S",
+                "gestorGrupo" => "S",
+                "gestorPrograma" => "S",
+            ],
+            [
+                "idLogin" => 6963,
+                "login" => "gestorUsuario",
+                "nomeUsuario" => "Gestor de Usuarios Example",
+                "email" => "super@admin.com",
+                "senha" => "1e5213b7cbc4d1780f09770123097879",
+                "validade" => "2025-02-27",
+                "idTotovs" => null,
+                "trocaSenha" => 0,
+                "validadeSenha" => "2025-02-27",
+                "token" => null,
+                "gestorUsuario" => "S",
+                "gestorGrupo" => "N",
+                "gestorPrograma" => "N",
+            ],
+            [
+                "idLogin" => 6964,
+                "login" => "gestorGrupo",
+                "nomeUsuario" => "gestorGrupo",
+                "email" => "super@admin.com",
+                "senha" => "1e5213b7cbc4d1780f09770123097879",
+                "validade" => "2025-02-27",
+                "idTotovs" => null,
+                "trocaSenha" => 0,
+                "validadeSenha" => "2025-02-27",
+                "token" => null,
+                "gestorUsuario" => "N",
+            ],
+            [
+                "idLogin" => 6964,
+                "login" => "Iron Main",
+                "nomeUsuario" => "Iron Main",
+                "email" => "super@admin.com",
+                "senha" => "1e5213b7cbc4d1780f09770123097879",
+                "validade" => "2025-02-27",
+                "idTotovs" => null,
+                "trocaSenha" => 0,
+                "validadeSenha" => "2025-02-27",
+                "token" => null,
+                "gestorUsuario" => "N",
+            ],
+            [
+                "idLogin" => 6964,
+                "login" => " Y",
+                "nomeUsuario" => "Black",
+                "email" => "super@admin.com",
+                "senha" => "1e5213b7cbc4d1780f09770123097879",
+                "validade" => "2025-02-27",
+                "idTotovs" => null,
+                "trocaSenha" => 0,
+                "validadeSenha" => "2025-02-27",
+                "token" => null,
+                "gestorUsuario" => "N",
+            ]
+        ];
+        
+
+        $this->loadTemplate('foo', $myapi);   
         exit;
     }
 
--- projects/MVC/PHP_MVC_StarterVueJS/views/foo.php
+++ projects/MVC/PHP_MVC_StarterVueJS/views/foo.php
@@ -1,15 +1,34 @@
 <div id="foo">
-
   <h2>{{message}}</h2>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cumque in laudantium vero</p>
 
   <button @click="handlerClick">handlerClick</button>
 
   <ul>
-    <li v-for="value in getRes" :key="value">
+    <li v-for="value in getRes" :key="value" @click="get_items">
      {{value.idLogin}} -  {{value.nomeUsuario}}
     </li>
   </ul>
+  <hr>
+
+  <?php if($data) :?>
+  <?php foreach($data as $val) :?>
+
+    <li @click="get_items('<?= $val['nomeUsuario']?>')"><?= $val['nomeUsuario']?></li>
+  <?php endforeach ?> 
+<?php endif ?>
+
+
+<hr>
+
+
+  <?php if($data) :?>
+  <?php foreach($data as $val) :?>
+
+    <li @click="handlerClick"><?= $val['nomeUsuario']?></li>
+  <?php endforeach ?> 
+<?php endif ?>
+
 </div>
 
 <script>
@@ -28,8 +47,13 @@ const { createApp, ref, computed, watch, reactive, onMounted } = Vue
     const getRes = ref()
 
 
-    function handlerClick() {
-      console.log(`hello`)
+    function handlerClick(e) {
+      console.log(e.target.textContent)
+    }
+
+    function get_items(e){
+      const selel = e.target;
+       console.log(selel);
     }
 
     onMounted(() => {
@@ -50,7 +74,7 @@ const { createApp, ref, computed, watch, reactive, onMounted } = Vue
     return {
       message,
       person,
-      handlerClick, getRes
+      handlerClick, getRes, get_items
     }
   },
   //template: '<p>Hello</p>',
