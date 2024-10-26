02f40e5f6495c626562b4e3e9eabfaa1151941fc
gmapdell <geraldo.filho92@hotmail.com>
2024-10-26 17:27:38 -0300

    UPDATE

--- projects/MVC/PHP_MVC_Starter-DEV-Tests/views/checkbox.php
+++ projects/MVC/PHP_MVC_Starter-DEV-Tests/views/checkbox.php
@@ -12,3 +12,26 @@ foreach($list as $opt): ?>
 <? endforeach ?>
   <button type="submit">Submit</button>
 </form>
+
+<hr>
+
+<?php
+// Definindo o array $erpOptions com dados fictícios das empresas
+$erpOptions = [
+  ['idEmpresa' => 1, 'idLegEmpresa' => 'Empresa 1'],
+  ['idEmpresa' => 2, 'idLegEmpresa' => 'Empresa 2'],
+  ['idEmpresa' => 3, 'idLegEmpresa' => 'Empresa 3'],
+  // Adicione mais empresas conforme necessário
+];
+?>
+
+<h1>Vinculação de Usuário às Instâncias ERP</h1>
+<form action="processar_formulario.php" method="POST">
+      <?php foreach ($erpOptions as $value) : ?>
+          <div class="form-check col-md-12" name="vinculaUsuarioInstanciaEdit">
+              <input type="checkbox" name="vinculaUsuarioInstanciaEdit[]" id="vinculaUsuarioInstancia<?php echo $value['idEmpresa']; ?>" class="form-check-input erpOptionCheckbox" value="<?php echo $value['idEmpresa']; ?>">
+              <label class="form-check-label" for="vinculaUsuarioInstancia<?php echo $value['idEmpresa']; ?>"><?php echo $value['idLegEmpresa']; ?></label>
+          </div>
+      <?php endforeach; ?>
+      <button type="submit" class="btn btn-primary mt-3">Enviar</button>
+</form>
--- projects/MVC/PHP_MVC_Starter-DEV-Tests/views/implodeAndExplode.php
+++ projects/MVC/PHP_MVC_Starter-DEV-Tests/views/implodeAndExplode.php
@@ -69,7 +69,17 @@
   foreach($list as $ind => $row){
       print_r("<p>{$row} - {$ind}</p>");  
   }  
+
+
+  $url = 'site.com/geraldo';
+
+  echo explode('/', $url)[0];  // site.com
+  echo '<br>';
+  echo explode('/', $url)[1]; // geraldo
+
   ?>
+
+  
   
 </body>
 </html>
