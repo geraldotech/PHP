09e09df9a58c383db7bc9f687b8ff4b3c95da574
gmapdell <geraldo.filho92@hotmail.com>
2025-01-11 20:14:56 -0300

    UPDATE

---
+++ General/Filesystem/CONSTANTS.php
@@ -0,0 +1,35 @@
+<style>
+  p{
+    border-top: 1px solid;
+  }
+</style>
+<?php
+/* DEFININDO CONST PARA O DOCUMENT ROOT */
+echo '<p>BASE_PATH</p>';
+define('BASE_PATH', $_SERVER['DOCUMENT_ROOT']);
+
+$local = BASE_PATH . '/PHP/General/Filesystem/exist.zip';
+print_r(BASE_PATH);
+
+echo '<p>file_exists local</p>';
+echo file_exists($local);
+
+echo '<p>getcws()</p>';
+/* USING getcwd para pegar o diretorio atual */
+$local_2 = getcwd() . '/exist.zip';
+echo getcwd() . "\n";
+
+
+echo '<p>file_exists local2</p>';
+echo file_exists($local_2);
+echo $local_2;
+
+
+echo '<p>__DIR__</p>';
+print_r(__DIR__);
+
+
+/* The full path and filename of the file with symlinks resolved */
+echo '<p>__FILE__</p>';
+print_r(__FILE__);
+
