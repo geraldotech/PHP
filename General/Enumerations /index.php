f56f4f7462e094c259d4181f5f5a4ae75d3beb64
Geraldo <geraldo.filho92@hotmail.com>
2026-02-08 15:12:57 -0300

    Contextualializando Enumerations

---
+++ General/Enumerations /index.php
@@ -0,0 +1,28 @@
+<?php
+enum Suit: int {
+  case Hearts = 1;
+  case Diamonds  = 2;
+  case Clubs = 3;
+  case Spades = 4;
+
+  function do_stuff(Suit $s) {
+    // ..
+    echo 'do_stuff';
+  }
+
+  function descricao(): string {
+    return match ($this) {
+      self::Hearts => 'coração',
+      self::Diamonds => 'diamentes',
+      self::Clubs => 'clube',
+      self::Spades => 'espadas',
+    };
+  }
+}
+
+$idRegraValida = Suit::Hearts->value;
+$desc = Suit::Hearts->descricao();
+echo $idRegraValida;
+echo '<br>';
+echo $desc;
+// do_stuff(Suit::Spades);
