53df135e0ee8d0864f6e42127af7b018cf83dfaf
Geraldo <geraldo.filho92@hotmail.com>
2026-03-21 10:19:09 -0300

    UPDATE

--- projects/MVC/PHP_MVC_Starter-DEV-Tests/models/TryCatchThrowable.php
+++ projects/MVC/PHP_MVC_Starter-DEV-Tests/models/TryCatchThrowable.php
@@ -6,31 +6,25 @@ class TryCatchThrowable extends Model {
   }
 
   /**
-   *  exemplo de try catch com Throwable
+   * exemplo de try catch com Throwable
    */
   public function exampleThrowable() {
-
     try {
+      $stmt = $this->db->prepare("
+            SELECT * 
+            FROM lgn_logins 
+            WHERE idLogin = :id
+        ");
 
-      /* === CONSULTA DENTRO DA TRANSACAO ===  */
-      $stmt = "SELECT * FROM lgn_logins WHERE idLogin = :id";
-      $stmt = $this->db->prepare($stmt);
       $stmt->execute([':id' => 2400]);
-      // $res = $stmt->rowCount();
       $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      if ($result) {
-        return $result;
-      }
-      //var_dump($res);
-
+      return $result ?: [];
+      
     } catch (\Throwable $t) {
       throw new \RuntimeException("Erro ao executar operação", 0, $t);
     }
   }
 
-  /**
-   * 
-   */
   public function exampleThrowableINSERT() {
     try {
 
@@ -59,7 +53,7 @@ class TryCatchThrowable extends Model {
       $res = $stmt->execute([
         ':notescol' => 'texto',
         ':age' => 'GeraldoDev2'
-      ]);      
+      ]);
 
       if ($res) {
         $lastId = $this->db->lastInsertId();
