dfb62704006920ae5fbf559baf050f5306f57dc6
Geraldo <geraldo.filho92@hotmail.com>
2026-05-09 15:34:28 -0300

    melhoria no index

--- projects/MVC/PHP_MVC_Starter-DEV-Tests/index.php
+++ projects/MVC/PHP_MVC_Starter-DEV-Tests/index.php
@@ -2,35 +2,28 @@
 // session_save_path('tmp');
 date_default_timezone_set('America/Sao_Paulo');
 session_start();
-//require 'vendor/autoload.php';
 require 'config.php';
 ini_set('memory_limit', '-1');
 // Reporta todos os erros
 ini_set('display_errors', 1);
 ini_set('display_startup_erros', 1);
-error_reporting(E_ALL);
+error_reporting(0);
 set_time_limit(50000000);
+
 // Reporta apenas erros fatais, noticias e erros de compilação
 // ini_set('display_errors',1); ini_set('display_startup_erros',1); error_reporting(E_ERROR | E_PARSE | E_NOTICE);
-
 spl_autoload_register(function ($class) {
-        if (file_exists('controllers/' . $class . '.php')) {
-                require_once 'controllers/' . $class . '.php';
-        } elseif (file_exists('models/' . $class . '.php')) {
-                require_once 'models/' . $class . '.php';
-        } elseif (file_exists('core/' . $class . '.php')) {
-                require_once 'core/' . $class . '.php';
-        } elseif (file_exists('regrasform/' . $class . '.php')) {
-                require_once 'regrasform/' . $class . '.php';
-        } elseif (file_exists('helper/' . $class . '.php')) {
-                require_once 'helper/' . $class . '.php';
+        $directories = ['controllers', 'models', 'core', 'regrasform', 'helper'];
+
+        foreach ($directories as $directory) {
+                $file = $directory . '/' . $class . '.php';
+
+                if (file_exists($file)) {
+                        require_once $file;
+                        return;
+                }
         }
 });
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 
 $core = new Core();
 $core->run();
